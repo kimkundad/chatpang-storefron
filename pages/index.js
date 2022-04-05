4867195bea04d7fe22bcb98f70cf0fcd69e58729
Bookaso <lohamat.s@gmail.com>
2022-04-06 00:19:14 +0700

    book 1st push

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import MyApp from './_app'
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <button type="button" className="btn btn-primary">Primary</button>
-    </div>
+    <MyApp>
+      <div className={styles.container}>
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
+    </MyApp>
   )
 }
