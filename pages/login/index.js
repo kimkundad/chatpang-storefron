a85d01701fe4d66676ed057c070a7243fd18c18f
bookaso <lohamat.s@gmail.com>
2022-08-16 10:12:21 +0700

    Fixed auth in login

--- pages/login/index.js
+++ pages/login/index.js
@@ -52,7 +52,7 @@ const Login = () => {
     let id = ''
     console.log(userId);
     try {
-      const res = await axios.get(`/public/facebook-users/${userId}`)
+      const res = !userId && await axios.get(`/public/facebook-users/${userId}`)
       // console.log(res.data.data);
       // const { facebook_id } = res.data.data
       // setUserData({ ...user,user:res.data.data, facebookUserId : facebook_id })
